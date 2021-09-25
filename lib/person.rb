44ca6a6b6f9aed566c0d6bd526e475207f754338
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-24 20:36:17 -0400

    commit tree e099412415bab2b26b85744ef18b891be5d60e42

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,52 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = if num > 10
+            10
+        elsif num < 0
+            0
+        else num
+        end
+    end
+   
+    def hygiene=(num)
+        @hygiene = if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    def happy?
+        happiness > 7 ? true : false
+    end 
+
+    def clean?
+        hygiene > 7 ? true : false
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    
+end
